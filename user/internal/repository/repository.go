00761f2add2f07df285948cbfcc943df16f1e0c6
Roman Horishny <horishny@proton.me>
2024-01-26 15:39:56 +0200

    refactor: update method instead of change password

--- user/internal/repository/repository.go
+++ user/internal/repository/repository.go
@@ -8,5 +8,5 @@ import (
 type UserRepository interface {
 	Create(ctx context.Context, user *model.User) error
 	Get(ctx context.Context, email string) (*model.User, error)
-	ChangePassword(ctx context.Context, email, password string) error
+	Update(ctx context.Context, user *model.User) error
 }
