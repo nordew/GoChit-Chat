bb4ef1c50031096e75815fd4b62cf6403bb4e2ef
Roman Horishny <horishny@proton.me>
2024-01-26 15:48:08 +0200

    feat: filter struct to get user by id/email + integrated into get method

--- user/internal/repository/repository.go
+++ user/internal/repository/repository.go
@@ -2,11 +2,17 @@ package repository
 
 import (
 	"context"
+	"github.com/google/uuid"
 	"user/internal/model"
 )
 
+type GetFilter struct {
+	ID    uuid.UUID
+	Email string
+}
+
 type UserRepository interface {
 	Create(ctx context.Context, user *model.User) error
-	Get(ctx context.Context, email string) (*model.User, error)
+	Get(ctx context.Context, filter *GetFilter) (*model.User, error)
 	Update(ctx context.Context, user *model.User) error
 }
