fdba41d54f94a28cc4f538a0da2f84e617de3007
Roman Horishny <horishny@proton.me>
2024-01-27 23:19:12 +0200

    refactor: User ID field int -> string, role string -> int

--- user/internal/repository/user/model/user.go
+++ user/internal/repository/user/model/user.go
@@ -1,24 +1,18 @@
 package model
 
 import (
-	"github.com/google/uuid"
 	"time"
 )
 
 type Role string
 
-const (
-	AdminRole Role = "ADMIN"
-	UserRole  Role = "USER"
-)
-
 type User struct {
-	ID           uuid.UUID `db:"id"`
+	ID           string    `db:"id"`
 	Name         string    `db:"name"`
 	Email        string    `db:"email"`
 	Password     string    `db:"password"`
 	RefreshToken string    `db:"refresh_token"`
-	Role         Role      `db:"role"`
+	Role         int       `db:"role"`
 	CreatedAt    time.Time `db:"created_at"`
 	UpdatedAt    time.Time `db:"updated_at"`
 }
