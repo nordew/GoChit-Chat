96fe77ebc3de0b62a7f0c5a63894aedbaedfc525
Roman Horishny <horishny@proton.me>
2024-01-27 23:22:48 +0200

    feat: Updating method implemented

---
+++ user/internal/api/user/update.go
@@ -0,0 +1,25 @@
+package user
+
+import (
+	"context"
+	desc "github.com/nordew/GoChitChat-External/gen/go/user"
+	"go.uber.org/zap"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"user/internal/api/user/dao"
+)
+
+func (i *Implementation) Update(ctx context.Context, req *desc.UpdateUserRequest) (*emptypb.Empty, error) {
+	const op = "user.Implementation.Update"
+
+	converted := dao.ToUserFromUpdateRequest(req)
+
+	i.log.Info("user converted: ", zap.Any("user", converted))
+
+	err := i.userService.Update(ctx, converted)
+	if err != nil {
+		i.log.Error("error updating user", zap.Error(err), zap.String("op", op))
+		return nil, err
+	}
+
+	return &emptypb.Empty{}, nil
+}
