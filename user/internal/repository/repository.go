6e06c24889f6546dd5bac75403ea282fc6a2f135
Roman Horishny <horishny@proton.me>
2024-01-27 23:18:23 +0200

    feat: Defined CheckIfEmailExits, Delete methods; refactor: Get method now accepts id

--- user/internal/repository/repository.go
+++ user/internal/repository/repository.go
@@ -5,13 +5,10 @@ import (
 	"user/internal/model"
 )
 
-type GetFilter struct {
-	ID    string
-	Email string
-}
-
 type UserRepository interface {
 	Create(ctx context.Context, user *model.User) error
-	Get(ctx context.Context, filter *GetFilter) (*model.User, error)
+	CheckIfEmailExists(ctx context.Context, email string) (bool, error)
+	Get(ctx context.Context, id string) (*model.User, error)
 	Update(ctx context.Context, user *model.User) error
+	Delete(ctx context.Context, id string) error
 }
