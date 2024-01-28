23b977f20dc240aac0544195dd5beddfe4da4255
Roman Horishny <horishny@proton.me>
2024-01-28 15:08:24 +0200

    refactor: Method for checking email removed

--- user/internal/repository/repository.go
+++ user/internal/repository/repository.go
@@ -7,7 +7,6 @@ import (
 
 type UserRepository interface {
 	Create(ctx context.Context, user *model.User) error
-	CheckIfEmailExists(ctx context.Context, email string) (bool, error)
 	Get(ctx context.Context, id string) (*model.User, error)
 	Update(ctx context.Context, user *model.User) error
 	Delete(ctx context.Context, id string) error
