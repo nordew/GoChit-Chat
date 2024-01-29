b4c65c83c1085869199e1718c61d65145b4c71e1
Roman Horishny <horishny@proton.me>
2024-01-29 16:27:08 +0200

    feat: Defined methods to get user by email/id and update refresh token

--- user/internal/repository/repository.go
+++ user/internal/repository/repository.go
@@ -7,7 +7,12 @@ import (
 
 type UserRepository interface {
 	Create(ctx context.Context, user *model.User) error
-	Get(ctx context.Context, id string) (*model.User, error)
+
+	GetByEmail(ctx context.Context, email string) (*model.User, error)
+	GetById(ctx context.Context, id string) (*model.User, error)
+
 	Update(ctx context.Context, user *model.User) error
+	UpdateRefreshToken(ctx context.Context, id string, refreshToken string) error
+
 	Delete(ctx context.Context, id string) error
 }
