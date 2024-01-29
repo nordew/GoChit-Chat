4f29992bf6de28929344cf471618e946a98e28c7
Roman Horishny <horishny@proton.me>
2024-01-29 16:25:29 +0200

    feat: Login method implemented

--- user/internal/service/user/get.go
+++ user/internal/service/user/get.go
@@ -2,14 +2,20 @@ package user
 
 import (
 	"context"
-	"go.uber.org/zap"
+	"errors"
+	"fmt"
+	"time"
 	"user/internal/model"
+	"user/pkg/auth"
+	userErrors "user/pkg/user_errors"
+
+	"go.uber.org/zap"
 )
 
 func (u userService) Get(ctx context.Context, email string) (*model.User, error) {
 	const op = "userService.Get"
 
-	user, err := u.userRepo.Get(ctx, email)
+	user, err := u.userRepo.GetByEmail(ctx, email)
 
 	if err != nil {
 		u.log.Error("error getting user", zap.Error(err), zap.String("op", op))
@@ -17,4 +23,50 @@ func (u userService) Get(ctx context.Context, email string) (*model.User, error)
 	}
 
 	return user, nil
+}
+
+func (u userService) Login(ctx context.Context, email string, password string) (string, string, error) {
+	const op = "userService.Login"
+
+	valid := IsValidEmail(email)
+	if !valid {
+		return "", "", userErrors.ErrInvalidEmail
+	}
+
+	user, err := u.userRepo.GetByEmail(ctx, email)
+	if err != nil {
+		if errors.Is(err, userErrors.ErrWrongEmailOrPassword) {
+			return "", "", fmt.Errorf("dolbaeb")
+		}
+		u.log.Error("error getting user", zap.Error(err), zap.String("op", op))
+		return "", "", err
+	}
+
+	err = u.hasher.Compare(user.Password, password)
+	if err != nil {
+		u.log.Error("error comparing password", zap.Error(err), zap.String("op", op))
+		return "", "", userErrors.ErrWrongEmailOrPassword
+	}
+
+	accessToken, refreshToken, err := u.auth.GenerateTokens(&auth.GenerateTokenClaimsOptions{
+		UserId: user.ID,
+	})
+	if err != nil {
+		u.log.Error("error generating token", zap.Error(err), zap.String("op", op))
+		return "", "", err
+	}
+
+	var userWithUpdatedTokens = &model.User{
+		ID:           user.ID,
+		RefreshToken: refreshToken,
+		UpdatedAt:    time.Now(),
+	}
+
+	err = u.userRepo.Update(ctx, userWithUpdatedTokens)
+	if err != nil {
+		u.log.Error("error updating user", zap.Error(err), zap.String("op", op))
+		return "", "", err
+	}
+
+	return accessToken, refreshToken, nil
 }
