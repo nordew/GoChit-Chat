f6249d7db183041fbb7b480e6c29ef1e31bbf167
Roman Horishny <horishny@proton.me>
2024-01-29 16:25:16 +0200

    feat: Login method implemented

--- user/internal/api/user/get.go
+++ user/internal/api/user/get.go
@@ -2,9 +2,10 @@ package user
 
 import (
 	"context"
+	"user/internal/api/user/dao"
+
 	desc "github.com/nordew/GoChitChat-External/gen/go/user"
 	"go.uber.org/zap"
-	"user/internal/api/user/dao"
 )
 
 func (i *Implementation) Get(ctx context.Context, req *desc.GetUserRequest) (*desc.GetUserResponse, error) {
@@ -26,6 +27,23 @@ func (i *Implementation) Get(ctx context.Context, req *desc.GetUserRequest) (*de
 		Role:      converted.Role,
 		CreatedAt: converted.CreatedAt,
 		UpdatedAt: converted.UpdatedAt,
+	}
+
+	return resp, nil
+}
+
+func (i *Implementation) Login(ctx context.Context, req *desc.LoginUserRequest) (*desc.LoginUserResponse, error) {
+	const op = "user.Implementation.Login"
+
+	accessToken, refreshToken, err := i.userService.Login(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		i.log.Error("error logging in user", zap.Error(err), zap.String("op", op))
+		return nil, err
+	}
+
+	resp := &desc.LoginUserResponse{
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
 	}
 
 	return resp, nil
