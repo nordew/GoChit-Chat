417ba39bd1d266b1120b4c011ebe21c2431b2b1f
Roman Horishny <horishny@proton.me>
2024-01-29 16:28:53 +0200

    feat: Defined login method, update request and response struct's. refactor: update method arguments

--- user/internal/service/service.go
+++ user/internal/service/service.go
@@ -8,14 +8,27 @@ import (
 // UserService defines the interface for the user service
 type UserService interface {
 	// Create creates a new user and returns its id
-	Create(ctx context.Context, user *model.User) (string, error)
+	Create(ctx context.Context, user *model.User) (*CreateUserResponse, error)
 
 	// Get returns a user by id
 	Get(ctx context.Context, email string) (*model.User, error)
 
 	// Update updates a user
-	Update(ctx context.Context, user *model.UserUpdate) error
+	Update(ctx context.Context, req *UpdateUserRequest) error
 
 	// Delete deletes a user by id
 	Delete(ctx context.Context, id string) error
+
+	// Login logs in a user
+	Login(ctx context.Context, email string, password string) (string, string, error)
+}
+
+type CreateUserResponse struct {
+	Id           string
+	AccessToken  string
+	RefreshToken string
+}
+
+type UpdateUserRequest struct {
+	User *model.UserUpdate
 }
