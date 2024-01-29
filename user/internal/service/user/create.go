cdb5128eae919e2449abfa87556c17fb6cd46532
Roman Horishny <horishny@proton.me>
2024-01-29 16:24:53 +0200

    feat: Adding tokens to db and response

--- user/internal/service/user/create.go
+++ user/internal/service/user/create.go
@@ -7,22 +7,24 @@ import (
 	"go.uber.org/zap"
 	"time"
 	"user/internal/model"
+	"user/internal/service"
+	"user/pkg/auth"
 	userErrors "user/pkg/user_errors"
 )
 
-func (u *userService) Create(ctx context.Context, user *model.User) (string, error) {
+func (u *userService) Create(ctx context.Context, user *model.User) (*service.CreateUserResponse, error) {
 	const op = "userService.Create"
 
 	err := validateUser(user.Name, user.Email, user.Password)
 	if err != nil {
 		u.log.Error("error validating user", zap.Error(err), zap.String("op", op))
-		return "", err
+		return nil, err
 	}
 
 	hashedPassword, err := u.hasher.Hash(user.Password)
 	if err != nil {
 		u.log.Error("error hashing password", zap.Error(err), zap.String("op", op))
-		return "", err
+		return nil, err
 	}
 
 	now := time.Now()
@@ -40,12 +42,31 @@ func (u *userService) Create(ctx context.Context, user *model.User) (string, err
 	err = u.userRepo.Create(ctx, parsedUser)
 	if err != nil {
 		if errors.Is(err, userErrors.ErrEmailAlreadyExists) {
-			return "", userErrors.ErrEmailAlreadyExists
+			return nil, userErrors.ErrEmailAlreadyExists
 		}
 
 		u.log.Error("error creating user", zap.Error(err), zap.String("op", op))
-		return "", err
+		return nil, err
 	}
 
-	return id.String(), nil
+	accessToken, refreshToken, err := u.auth.GenerateTokens(&auth.GenerateTokenClaimsOptions{
+		UserId: id.String(),
+	})
+	if err != nil {
+		u.log.Error("error generating token", zap.Error(err), zap.String("op", op))
+		return nil, err
+	}
+
+	err = u.userRepo.UpdateRefreshToken(ctx, id.String(), refreshToken)
+	if err != nil {
+		u.log.Error("error updating refresh token", zap.Error(err), zap.String("op", op))
+	}
+
+	resp := &service.CreateUserResponse{
+		Id:           id.String(),
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+	}
+
+	return resp, nil
 }
