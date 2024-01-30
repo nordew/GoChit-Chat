2cdd6b8b3136b9c5d2bb7b7be5496f79bbc81568
Roman Horishny <horishny@proton.me>
2024-01-30 18:33:00 +0200

    refactor: Defined custom errors for methods

--- user/internal/service/service.go
+++ user/internal/service/service.go
@@ -3,24 +3,25 @@ package service
 import (
 	"context"
 	"user/internal/model"
+	userErrors "user/pkg/user_errors"
 )
 
 // UserService defines the interface for the user service
 type UserService interface {
 	// Create creates a new user and returns its id
-	Create(ctx context.Context, user *model.User) (*CreateUserResponse, error)
+	Create(ctx context.Context, user *model.User) (*CreateUserResponse, *userErrors.CustomErr)
 
 	// Get returns a user by id
-	Get(ctx context.Context, email string) (*model.User, error)
+	Get(ctx context.Context, email string) (*model.User, *userErrors.CustomErr)
 
 	// Update updates a user
-	Update(ctx context.Context, req *UpdateUserRequest) error
+	Update(ctx context.Context, req *UpdateUserRequest) *userErrors.CustomErr
 
 	// Delete deletes a user by id
-	Delete(ctx context.Context, id string) error
+	Delete(ctx context.Context, id string) *userErrors.CustomErr
 
 	// Login logs in a user
-	Login(ctx context.Context, email string, password string) (string, string, error)
+	Login(ctx context.Context, email string, password string) (string, string, *userErrors.CustomErr)
 }
 
 type CreateUserResponse struct {
