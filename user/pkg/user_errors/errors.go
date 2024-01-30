5777a005f517cfc46c7acc839480d64f0032c64b
Roman Horishny <horishny@proton.me>
2024-01-30 18:32:45 +0200

    feat: Constructors for custom errors

--- user/pkg/user_errors/errors.go
+++ user/pkg/user_errors/errors.go
@@ -1,6 +1,9 @@
 package userErrors
 
-import "errors"
+import (
+	"errors"
+	"google.golang.org/grpc/codes"
+)
 
 var (
 	ErrEmailAlreadyExists   = errors.New("email already exists")
@@ -9,3 +12,33 @@ var (
 	ErrInvalidPassword      = errors.New("invalid password")
 	ErrWrongEmailOrPassword = errors.New("wrong email or password")
 )
+
+type CustomErr struct {
+	Err  error
+	Msg  string
+	Code codes.Code
+}
+
+func New(err error, msg string, code codes.Code) *CustomErr {
+	return &CustomErr{
+		Err:  err,
+		Msg:  msg,
+		Code: code,
+	}
+}
+
+func NewInternalErr(err error) *CustomErr {
+	return &CustomErr{
+		Err:  err,
+		Msg:  "internal sever error",
+		Code: codes.Internal,
+	}
+}
+
+func PasswordOrEmailMismatch(err error) *CustomErr {
+	return &CustomErr{
+		Err:  err,
+		Msg:  ErrWrongEmailOrPassword.Error(),
+		Code: codes.NotFound,
+	}
+}
