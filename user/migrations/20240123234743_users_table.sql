920352c165f9ab2ad645d885d5d0c9caff022acd
Roman Horishny <horishny@proton.me>
2024-02-05 17:24:13 +0200

    refactor: Name and password fields max length: 30

--- user/migrations/20240123234743_users_table.sql
+++ user/migrations/20240123234743_users_table.sql
@@ -2,9 +2,9 @@
 -- +goose StatementBegin
 CREATE TABLE user_table (
     id UUID PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(30) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(30) NOT NULL,
     refresh_token VARCHAR NOT NULL DEFAULT '',
     role INT NOT NULL DEFAULT 0,
     created_at TIMESTAMP DEFAULT current_timestamp,
