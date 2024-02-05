e4f0129fa9cd82886a934e0c818619e3cee3b66a
Roman Horishny <horishny@proton.me>
2024-02-05 17:32:28 +0200

    feat: Implementation of GetByID and GetAllUsersFromRoom method's

---
+++ chat/internal/repository/room/get.go
@@ -0,0 +1,75 @@
+package room
+
+import (
+	"chat/internal/model"
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+func (r roomRepository) GetByID(ctx context.Context, id string) (*model.Room, bool, error) {
+	room := &model.Room{}
+
+	query := "SELECT room_id, room_name FROM rooms WHERE room_id = $1"
+	err := r.conn.QueryRow(ctx, query, id).Scan(&room.ID, &room.Name)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, false, fmt.Errorf("room doesn't exist")
+		}
+		return nil, false, err
+	}
+
+	query = "SELECT user_id FROM user_to_room WHERE room_id = $1"
+	rows, err := r.conn.Query(ctx, query, id)
+	if err != nil {
+		return nil, false, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var userID string
+		err := rows.Scan(&userID)
+		if err != nil {
+			return nil, false, err
+		}
+
+		user := model.User{ID: userID}
+		room.Users = append(room.Users, user)
+	}
+
+	return room, true, nil
+}
+
+func (r roomRepository) GetAllUsersFromRoom(ctx context.Context, roomID string) ([]model.User, error) {
+	const op = "roomRepo.GetAllUsersFromRoom"
+
+	query := `
+		SELECT u.user_id, u.username
+		FROM users u
+		INNER JOIN user_to_room ur ON u.user_id = ur.user_id
+		WHERE ur.room_id = $1
+	`
+
+	rows, err := r.conn.Query(ctx, query, roomID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []model.User
+
+	for rows.Next() {
+		var user model.User
+		err := rows.Scan(&user.ID, &user.Username)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
