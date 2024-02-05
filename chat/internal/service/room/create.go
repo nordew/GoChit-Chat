6c6f9743decbf341bdfa1b0868678777608b74d5
Roman Horishny <horishny@proton.me>
2024-02-05 17:50:55 +0200

    feat: Implemented create room method

---
+++ chat/internal/service/room/create.go
@@ -0,0 +1,29 @@
+package room
+
+import (
+	"chat/internal/model"
+	"chat/internal/service"
+	"context"
+	"github.com/google/uuid"
+)
+
+func (s *roomService) CreateRoom(ctx context.Context, req *service.CreateRoomReq) (string, error) {
+	const op = "roomService.CreateRoom"
+
+	id := uuid.New().String()
+
+	users := append([]model.User{}, model.User{Username: req.Username, ID: req.UserID})
+
+	room := &model.Room{
+		ID:    id,
+		Name:  req.RoomName,
+		Users: users,
+	}
+
+	if err := s.roomRepo.Create(ctx, room); err != nil {
+		s.logger.Error("failed to create room", op, err.Error())
+		//return "", service.ErrInternal
+	}
+
+	return id, nil
+}
