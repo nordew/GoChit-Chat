7e74b0a26207ae54cde1bfbc2121e2b041728585
Roman Horishny <horishny@proton.me>
2024-02-06 15:46:45 +0200

    feat: Defined GetByToken method

--- user/internal/repository/repository.go
+++ user/internal/repository/repository.go
@@ -9,6 +9,7 @@ type UserRepository interface {
 	Create(ctx context.Context, user *model.User) error
 	GetByEmail(ctx context.Context, email string) (*model.User, error)
 	GetById(ctx context.Context, id string) (*model.User, error)
+	GetByToken(ctx context.Context, token string) (*model.User, error)
 	Update(ctx context.Context, user *model.User) error
 	UpdateRefreshToken(ctx context.Context, id string, refreshToken string) error
 	Delete(ctx context.Context, id string) error
